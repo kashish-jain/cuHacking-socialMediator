6acf37c03c2ca71f689f930b23498d45e3f06210
Kashish Jain <kashish.emailme@gmail.com>
2020-01-12 04:37:50 -0500

    new try

--- frontend/my-app/src/components/post.jsx
+++ frontend/my-app/src/components/post.jsx
@@ -29,7 +29,7 @@ class Post extends Component {
       .post("http://127.0.0.1:5000/reviews';", {
         headers: {
             'Content-Type': 'application/json',
-         data}
+        },data
         })
       .then(res => console.log(res))
       .catch(err => console.log(err));

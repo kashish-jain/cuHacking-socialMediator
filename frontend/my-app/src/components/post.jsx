5a5b6c0e801a941517ec4e5d994bc5ec74a5351a
Kashish Jain <kashish.emailme@gmail.com>
2020-01-12 04:14:50 -0500

    post req front end done

---
+++ frontend/my-app/src/components/post.jsx
@@ -0,0 +1,53 @@
+
+import React, { Component } from "react";
+import axios from "axios";
+
+class Post extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  onTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  onBodyChange = e => {
+    this.setState({
+      body: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    axios
+      .post("http://127.0.0.1:5000/reviews';", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="post" style={{height: "50vw", width: "50vh"}}>
+        <form className="post" onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Title" value={this.state.title}
+            onChange={this.onTitleChange} required
+          />
+          <textarea
+            placeholder="Body" value={this.state.body}
+            onChange={this.onBodyChange} required
+          />
+          <button type="submit">Create Post</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Post;

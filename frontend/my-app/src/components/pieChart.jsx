bba20f670d78b2695757188f843219fb23b43c49
Kashish Jain <kashish.emailme@gmail.com>
2020-01-12 09:58:46 -0500

    new graph

---
+++ frontend/my-app/src/components/pieChart.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Pie } from 'react-chartjs-2';
+
+
+
+class PieChart extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        const data = {
+            labels: [
+                'Software',
+                'None',
+                'Calls/Telecom',
+                'ATM',
+                'Hours'
+            ],
+            datasets: [{
+                data: this.props.data,
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    '#FC2A3B',
+                    '#A3ABCC'
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    '#AABBCC',
+                    '#CAB123'
+                ]
+            }]
+        };
+        return (
+            <div style= {{width: "40vw"}}>
+                {/* <h2>Pie Example</h2> */}
+                <Pie data={data} />
+            </div>
+        );
+    }
+}
+
+
+
+
+export default PieChart;
